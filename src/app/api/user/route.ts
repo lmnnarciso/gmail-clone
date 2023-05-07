2302676819785feb74b466abe559664ec185892a
lmnnarciso <lmnnarciso14@gmail.com>
2023-05-08 00:34:23 +0800

    remove api handler

--- src/app/api/user/route.ts
+++ src/app/api/user/route.ts
@@ -1,13 +1,16 @@
-import type { NextApiRequest, NextApiResponse } from "next";
+// import type { NextApiRequest, NextApiResponse } from "next";
 
-// Fake users data
-const users: { id: number }[] = [{ id: 1 }, { id: 2 }, { id: 3 }];
+// // Fake users data
+// const users: { id: number }[] = [{ id: 1 }, { id: 2 }, { id: 3 }];
 
-export default function handler(
-  _req: NextApiRequest,
-  res: NextApiResponse<{ id: number }[]>
-) {
-  console.log("test");
-  // Get data from your database
-  res.status(200).json(users);
+// export default function handler(
+//   _req: NextApiRequest,
+//   res: NextApiResponse<{ id: number }[]>
+// ) {
+//   console.log("test");
+//   // Get data from your database
+//   res.status(200).json(users);
+// }
+export async function GET(request: Request) {
+  return new Response("Hello, Next.js!");
 }
